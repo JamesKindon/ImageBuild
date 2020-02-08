aa1f164548cd911b38d5378f57aebf5d8b320213
JamesKindon <James.Kindon@gmail.com>
2020-02-08 17:06:39 +1100

    Update Phase3-Control-Optimize.ps1

--- Phase3-Control-Optimize.ps1
+++ Phase3-Control-Optimize.ps1
@@ -12,6 +12,7 @@ Set-ExecutionPolicy Bypass -Force
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/j81blog/Citrix_Optimizer_Community_Template_Marketplace/master/templates/John%20Billekens/JohnBillekens_3rd_Party_Components.xml" -UseBasicParsing -OutFile "C:\Tools\CitrixOptimizer\Templates\3rd_Party_Components.xml"
 & "C:\Tools\CitrixOptimizer\CtxOptimizerEngine.ps1" -Template "C:\Tools\CitrixOptimizer\Templates\3rd_Party_Components.xml" -mode Execute
 
+
 Switch -Regex ((Get-WmiObject Win32_OperatingSystem).Caption) {
     "Microsoft Windows Server*" {
     }
