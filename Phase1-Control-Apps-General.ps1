affdbf236e3dc7df7535ac67f001ee40af7d23f3
JamesKindon <James.Kindon@gmail.com>
2020-03-13 22:05:58 +1100

    Update Phase1-Control-Apps-General.ps1

--- Phase1-Control-Apps-General.ps1
+++ Phase1-Control-Apps-General.ps1
@@ -1,4 +1,9 @@
 #Phase 1 - Apps
+
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+
 Write-Host "====== Install 7zip\"
 choco install 7zip.install -Y
 
