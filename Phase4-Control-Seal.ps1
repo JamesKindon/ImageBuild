e73be6c73faf080ca8a9d7203abf68a8874c1820
James Kindon <James.Kindon@gmail.com>
2021-03-01 12:06:00 +1100

    Update Phase4-Control-Seal.ps1

--- Phase4-Control-Seal.ps1
+++ Phase4-Control-Seal.ps1
@@ -2,17 +2,37 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/JamesKindon/ImageBuild/master/Get-InstalledSoftware.ps1'))
 
-Write-Host "====== Enable Windows Defender real time scan\"
-Set-MpPreference -DisableRealtimeMonitoring $false -ErrorAction SilentlyContinue
+# Re-enable Defender
+Write-Output "====== Enable Windows Defender real time scan"
+Set-MpPreference -DisableRealtimeMonitoring $false
+Write-Output "====== Enable Windows Store updates"
+reg delete HKLM\Software\Policies\Microsoft\Windows\CloudContent /v DisableWindowsConsumerFeatures /f
+reg delete HKLM\Software\Policies\Microsoft\WindowsStore /v AutoDownload /f
 
-Write-Host "====== Sysprep\"
+# Sysprep
+Write-Output "====== Run Sysprep"
+
+#region Prepare
+If (Get-Service -Name "RdAgent" -ErrorAction "SilentlyContinue") { Set-Service -Name "RdAgent" -StartupType "Disabled" }
+If (Get-Service -Name "WindowsAzureTelemetryService" -ErrorAction "SilentlyContinue") { Set-Service -Name "WindowsAzureTelemetryService" -StartupType "Disabled" }
+If (Get-Service -Name "WindowsAzureGuestAgent" -ErrorAction "SilentlyContinue") { Set-Service -Name "WindowsAzureGuestAgent" -StartupType "Disabled" }
+Remove-ItemProperty -Path 'HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Setup\\SysPrepExternal\\Generalize' -Name '*'
+#endregion
+
+#region Sysprep
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State"
 & $env:SystemRoot\System32\Sysprep\Sysprep.exe /oobe /generalize /quiet /quit
-while ($true) { 
-    $imageState = Get-ItemProperty HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\State | Select-Object ImageState
-    if ($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') { 
-        Write-Output $imageState.ImageState; Start-Sleep -s 10 
+While ($True) {
+    $imageState = Get-ItemProperty $RegPath | Select-Object ImageState
+    If ($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') {
+        Write-Output $imageState.ImageState
+        Start-Sleep -s 10 
     }
-    else { 
-        break 
-    } 
+    Else {
+        Break
+    }
 }
+$imageState = Get-ItemProperty $RegPath | Select-Object ImageState
+Write-Output $imageState.ImageState
+#endregion
+Write-Host "================ Complete: Sysprep."
