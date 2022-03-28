e1e3ebdf7bd98d79d53255b5bd1427043b177d0d
James Kindon <James.Kindon@gmail.com>
2022-03-28 21:11:41 +1100

    Updated Script Logic

--- ModularBuild/scripts/P3-App-Local-Bloomberg.ps1
+++ ModularBuild/scripts/P3-App-Local-Bloomberg.ps1
@@ -1,17 +1,58 @@
+<#
+.SYNOPSIS
+    Downloads and Installs Bloomberg
+.DESCRIPTION
+	Can be used as part of a pipeline or MDT task sequence.
+.EXAMPLE
+	.\P3-App-Local-Bloomberg.ps1 -UseScriptVariables
+	Will use hardcoded download details in script
 
-##---------------------------------------------------
-# Variables per application
-##---------------------------------------------------
-$Application = "Bloomberg"
-$DownloadFolder = "C:\Apps\Temp\"
-$AppInstallSource = "Bloomberg.zip"
-##---------------------------------------------------
-# Variables from vault (leave these alone)
-##---------------------------------------------------
-$Base = $env:sabaseappsdir
-$Key = $env:sasaskey
-$URI = $Base + $AppInstallSource + $Key
-##---------------------------------------------------
+	.\P3-App-Local-CitrixCQI-DL.ps1
+	Assumes pipeline environment variables
+#>
+
+#region Params
+# ============================================================================
+# Parameters
+# ============================================================================
+Param(
+	[Parameter(Mandatory = $false)]
+    [switch]$UseScriptVariables # Use script variables for downloads (not using environment variables)
+
+)
+#endregion
+
+#region Variables
+# ============================================================================
+# Variables
+# ============================================================================
+# Set Variables
+#//Release Data
+$Application        = "Bloomberg"
+$DownloadFolder     = "C:\Apps\Temp\"
+$AppInstallSource   = "Bloomberg.zip"
+#//Hardcoded Variables
+$Base_HC            = "FolderStoringAppsInStorageAccount" #Only used when UseScriptVariables Switch is present, else pipeline
+$Key_HC             = "KeytoAccessFolderInStorageAccount" #Only used when UseScriptVariables Switch is present, else pipeline
+#//Pipeline Variables
+$Base               = $env:sabaseappsdir
+$Key                = $env:sasaskey
+$URI                = $Base + $AppInstallSource + $Key
+#endregion
+
+#region Functions
+# ============================================================================
+# Functions
+# ============================================================================
+#endregion
+
+#Region Execute
+# ============================================================================
+# Execute
+# ============================================================================
+Write-Host "============================================================"
+Write-Host "===== Downloading $($Application)"
+Write-Host "============================================================"
 
 $ProgressPreference = 'SilentlyContinue'
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -20,9 +61,12 @@ if (!(Test-Path -Path $DownloadFolder)) {
     New-Item -Path $DownloadFolder -ItemType Directory | Out-Null
 }
 
-Write-Host "============================================================"
-Write-Host "===== Downloading $($Application)"
-Write-Host "============================================================"
+if ($UseScriptVariables.IsPresent) {
+	Write-Host "UseScriptVariables present - using hardcoded script values"
+	$Base   = $Base_HC
+    $Key    = $Key_HC
+}
+
 
 $dlparams = @{
     uri             = $URI
@@ -43,3 +87,5 @@ $params = @{
     PassThru     = $True
 }
 Start-Process @params
+
+#endregion
