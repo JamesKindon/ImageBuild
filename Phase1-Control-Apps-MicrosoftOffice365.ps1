e82c16395bb8336f98ee013c2c290d1ed22881da
James Kindon <James.Kindon@gmail.com>
2022-04-20 22:25:02 +1000

    cleanse

--- Phase1-Control-Apps-MicrosoftOffice365.ps1
+++
@@ -1,3 +0,0 @@
-Write-Host "====== Install Microsoft Office 365 ProPlus\"
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/JamesKindon/ImageBuild/master/Phase1-MicrosofOffice.ps1'))
